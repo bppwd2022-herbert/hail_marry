97f9a6f425f41878f658b84b03ecb2248d9e5279
Harrison Herbert <h.herbert@baypath.net>
2022-04-28 12:08:44 +0000

    at home day 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  #root to: "home#index"
+  devise_scope :user do
+    get "/users/sign_in" => "new_user_session_path"
+  end
+  root to: "devise/sessions#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
