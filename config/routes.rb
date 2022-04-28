4feebc99504732307d385e4575ddb59d37cdfd32
Harrison Herbert <h.herbert@baypath.net>
2022-04-28 13:27:52 +0000

    root error

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
+  get 'home/index'
   devise_for :users
+  
   devise_scope :user do
-    get "/users/sign_in" => "new_user_session_path"
+    root to: "devise/sessions#new"
   end
-  root to: "devise/sessions#create"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
